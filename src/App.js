a9bf8858e6422b7781034ec61c7adfd5905ffe5b
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2020-12-20 00:40:49 +0530

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>hi</div>
+      <Switch>
+        <Route exact path="/">
+          This is home page
+        </Route>
+        <Route>this is 404 page</Route>
+      </Switch>
     );
   }
 }
