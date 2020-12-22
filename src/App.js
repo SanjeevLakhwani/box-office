e5f25f2da6ebaf240e5a4b2baa1f52cea4840fbe
SanjeevLakhwani <lakhwani.sanjeev691@gmail.com>
2020-12-22 10:23:54 +0530

    added navbar and created page components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 class App extends Component {
   render() {
     return (
-      <Switch>
-        <Route exact path="/">
-          This is home page
-        </Route>
-        <Route>this is 404 page</Route>
-      </Switch>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/starred">
+            <Starred />
+          </Route>
+          <Route>this is 404 page</Route>
+        </Switch>
+      </div>
     );
   }
 }
